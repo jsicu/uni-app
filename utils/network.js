9343b60de783040a88af479fe405f98a389041ef
lzq741167479 <741167479@qq.com>
2020-12-01 14:00:08 +0800

    change: 网络请求封装小程序适配

--- utils/network.js
+++ utils/network.js
@@ -17,28 +17,24 @@ module.exports = (params) => {
 		method = method.toUpperCase(); //	小写转大写
 		if (method == "POST") {
 			header = {
-				"Content-Type": "application/x-www-form-urlencoded"
+				"content-type": "application/x-www-form-urlencoded"
 			}
 		}
 	}
-	uni.getStorage({
-		key: 'userInfo',
-		success: (res) => {
-			console.log(res.data);
-			header.token = res.data.token
-		},
-		fail: (err) => {
+	const WHITELIST = ['security/publicKey', 'security/login'];
+	// if (!WHITELIST.some(element => element === url)) {
+		try {
+			const value = uni.getStorageSync('userInfo')
+			if (value) {
+				header.token = value.token
+			}
+		} catch (e) {
 			uni.redirectTo({
-				// navigateTo redirectTo
 				url: '/pages/index/index'
 			});
 		}
-	});
-	// if () {
-
-	// } else{
-
 	// }
+	console.log(header, url)
 	//	发起请求 加载动画
 	if (!params.hideLoading) {
 		uni.showLoading({
@@ -52,7 +48,7 @@ module.exports = (params) => {
 		header: header,
 		data: data,
 		dataType: "json",
-		sslVerify: false, //	是否验证ssl证书
+		// sslVerify: false, //	是否验证ssl证书
 		success: res => {
 			if (res.statusCode && res.statusCode != 200) {
 				//	api错误
@@ -61,13 +57,15 @@ module.exports = (params) => {
 				})
 				return;
 			}
-			// if(res.data.code === 0) {
-			// 	//	api错误
-			// 	uni.showModal({
-			// 		content: res.data.message
-			// 	})
-			// 	return;
-			// }
+			if (res.data.code === 0) {
+				// 登录过期处理
+				uni.removeStorageSync('userInfo')
+				uni.redirectTo({
+					// navigateTo redirectTo
+					url: '/pages/index/index'
+				});
+				return;
+			}
 			typeof params.success == "function" && params.success(res.data.data);
 		},
 		fail: err => {
